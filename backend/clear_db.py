cc23e51f46d795d41b3c86e266b466fa6617c5c4
SyedAbbas119 <s.abbas9249@gmail.com>
2025-01-14 21:56:00 +0000

    I have added a comment

--- backend/clear_db.py
+++ backend/clear_db.py
@@ -1,6 +1,6 @@
 from configuration import db, app
 from models import Movie  
-
+#This file was created for testing purposes to clear the database
 with app.app_context():
     # Clear all records from the Movie table
     db.session.query(Movie).delete()
